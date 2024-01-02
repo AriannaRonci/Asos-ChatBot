33495eec35f2c7bc980b1754f394abc95fdc37b3
AliceMoretti00 <morettialice@outlook.it>
2024-01-02 15:52:11 +0100

    modifiche

--- actions/actions.py
+++ actions/actions.py
@@ -38,19 +38,24 @@ class ActionVisualizeProduct(Action):
         else:
             model = result['size and fit'].iloc[0]
             if model.startswith("Model wears"):
-                idx = model.index("Model's height")
-                model_size = model[:idx] + ', ' + model[idx:]
+                if "Model's height" in model:
+                    idx = model.index("Model's height")
+                    model = model[:idx] + ', ' + model[idx:]
             if model.startswith("Model's height"):
-                idx = model.index("Model wears")
-                model_size = model[:idx] + ', ' + model[idx:]
+                if "Model wears" in model:
+                    idx = model.index("Model wears")
+                    model = model[:idx] + ', ' + model[idx:]
 
             image_url = result['image'].iloc[0]
             title = result['category'].iloc[0]
-            string = "Here is the image for the product with sku " + sku + ".\n" + model_size
+            string = "Here is the image for the product with sku " + sku + "."
+            help_choice = "To make you feel even more confident in your choice, I'll provide you with some details" \
+                          "about the model.\n" + model + "."
 
             dispatcher.utter_message(text=string)
             dispatcher.utter_message(text=title)
             dispatcher.utter_message(image=image_url)
+            dispatcher.utter_message(text=help_choice)
 
         return [{"name": "sku", "event": "slot", "value": None}]
 
@@ -80,7 +85,7 @@ class GetInfoBySku(Action):
                                                           "By the way you have a really good taste in terms of fashion:"
 
                 dispatcher.utter_message(text=text, image=image)
-                dispatcher.utter_message("You can ask me to find you a specific color")
+                dispatcher.utter_message("You can ask me to find you a specific color.")
         except:
             dispatcher.utter_message("Sorry, the sku code you chose is not valid.")
 
@@ -105,15 +110,16 @@ class GetColors(Action):
                 (fashion_items['sku'] == int(sku)) & (fashion_items['color'].str.lower() == str(color))]
 
             if len(result) == 0:
-                text = "Sorry, this color is not available\n" \
-                       "The available colors are: " + fashion_items[fashion_items['sku'] == int(sku)]['color'].iloc[0]
+                text = "Sorry, this color is not available.\n" \
+                       "The available colors are: " + fashion_items[fashion_items['sku'] == int(sku)]['color'].iloc[0] \
+                       + "."
                 dispatcher.utter_message(text=text)
             else:
-                dispatcher.utter_message("Nice choice, this color is available")
+                dispatcher.utter_message("Nice choice, this color is available.")
         except:
             print('sono dentro except')
-            text = "Sorry, this color is not available\n" \
-                   "The available colors are: " + fashion_items[fashion_items['sku'] == int(sku)]['color'].iloc[0]
+            text = "Sorry, this color is not available.\n" \
+                   "The available colors are: " + fashion_items[fashion_items['sku'] == int(sku)]['color'].iloc[0] + "."
             dispatcher.utter_message(text=text)
 
         print('sono fuori da except')
@@ -136,10 +142,10 @@ class ValidateItemForm(FormValidationAction):
         result = fashion_items[fashion_items['sku'].astype(str) == str(sku)]
 
         if len(result) == 0:
-            dispatcher.utter_message(text="The sku code is not valid")
+            dispatcher.utter_message(text="The sku code is not valid.")
             return {'sku': None}
         else:
-            dispatcher.utter_message(text=f'Ok, you chose to buy the fashion item {sku}')
+            dispatcher.utter_message(text=f'Ok, you chose to buy the fashion item {sku}.')
             return {'sku': sku}
 
     def validate_color(
@@ -161,10 +167,11 @@ class ValidateItemForm(FormValidationAction):
         print(result)
 
         if color not in result['color'].iloc[0].lower():
-            dispatcher.utter_message(text="This color is not available for this fashion item")
-            dispatcher.utter_message(text="The colors available for this item are: " + result['color'].iloc[0].lower())
+            dispatcher.utter_message(text="This color is not available for this fashion item.")
+            dispatcher.utter_message(text="The colors available for this item are: " + result['color'].iloc[0].lower()
+                                          + ".")
             return {"color": None}
-        dispatcher.utter_message(text=f"Ok, you chose the color {color} for this fashion item")
+        dispatcher.utter_message(text=f"Ok, you chose the color {color} for this fashion item.")
         return {"color": color}
 
     def validate_size(
@@ -182,10 +189,11 @@ class ValidateItemForm(FormValidationAction):
         result = fashion_items.loc[(fashion_items['sku'] == int(sku))]
 
         if size not in result['size'].iloc[0].lower():
-            dispatcher.utter_message(text="This size is not available for this fashion item")
-            dispatcher.utter_message(text="The size available for this item are: " + result['size'].iloc[0].lower())
+            dispatcher.utter_message(text="This size is not available for this fashion item.")
+            dispatcher.utter_message(text="The size available for this item are: " + result['size'].iloc[0].lower()
+                                          + ".")
             return {"size": None}
-        dispatcher.utter_message(text=f"Ok, you chose the size {size} for this fashion item")
+        dispatcher.utter_message(text=f"Ok, you chose the size {size} for this fashion item.")
         return {"size": size}
 
 
@@ -205,8 +213,7 @@ class SubmitAcquisto(FormValidationAction):
         price = fashion_items.loc[(fashion_items['sku'] == int(sku))]['price'].iloc[0]
 
         message = "To buy it press this link and follow its instructions " + str(url) + ".\nIt will cost you " + str(
-            price) + \
-                  " dollars."
+            price) + " dollars."
         dispatcher.utter_message(text=message)
 
         return [{"name": "sku", "event": "slot", "value": None},
@@ -236,12 +243,12 @@ class GetCategorie(Action):
             cat = ''
             res = result.sample(n=5)
             for ind in res.index:
-                cat = cat + f' - ' + str(int(result["sku"][ind])) + ': ' + res["category"][ind] + '\n'
+                cat = cat + f' - ' + str(int(result["sku"][ind])) + ': ' + res["category"][ind] + '.\n'
 
             dispatcher.utter_message(text=f"I found {len(result)} results that match your input.\n"
                                           f"I will show you up to 5 fashion items that I think will fit you:\n" + cat
                                           + "\nIf you want to know more about some of these fashion items just tell me "
-                                            "'I want to know more about a specific fashion item.'")
+                                            "'I want to know more about a specific fashion item'.")
 
         return [{"name": "category", "event": "slot", "value": None}]
 
@@ -265,7 +272,7 @@ class GetWashDetails(Action):
             if len(result) == 0:
                 dispatcher.utter_message("This fashion item does not exist.")
             else:
-                string = str(result['look after me'].iloc[0])
+                string = str(result['look after me'].iloc[0]) + "."
                 dispatcher.utter_message(text=string)
                 return [{"name": "sku", "event": "slot", "value": None}]
         except:
@@ -289,7 +296,7 @@ class GetFabricDetails(Action):
             if len(result) == 0:
                 dispatcher.utter_message("This fashion item does not exist.")
             else:
-                string = str(result['about me'].iloc[0])
+                string = str(result['about me'].iloc[0]) + "."
                 dispatcher.utter_message(text=string)
                 return [{"name": "sku", "event": "slot", "value": None}]
         except:
@@ -299,29 +306,6 @@ class GetFabricDetails(Action):
 class ValidateDetailsForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_sku_color_form"
-
-    """def validate_category(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict):
-
-        category = tracker.get_slot('category').lower()
-
-        result = fashion_items[fashion_items['category'].str.lower().str.contains(category)]
-
-        result = result[['category', 'sku', 'url']]
-        if len(result) == 0:
-            dispatcher.utter_message("Sorry, I didn't find any fashion item that matches this category.")
-        else:
-            cat = ''
-            res = result.sample(n=5)
-            for ind in res.index:
-                cat = cat + f' - ' + str(int(result["sku"][ind])) + ': ' + res["category"][ind] + '\n'
-
-            dispatcher.utter_message(text=f"I found {len(result)} results that match your input.\n"
-                                          f"I will show you up to 5 fashion items that I think will fit you:\n" + cat)"""
 
     def validate_sku(
             self,
@@ -335,10 +319,10 @@ class ValidateDetailsForm(FormValidationAction):
         result = fashion_items[fashion_items['sku'].astype(str) == str(sku)]
 
         if len(result) == 0:
-            dispatcher.utter_message(text="The sku code is not valid")
+            dispatcher.utter_message(text="The sku code is not valid.")
             return {'sku': None}
         else:
-            dispatcher.utter_message(text=f'Ok, you chose to buy the fashion item {sku}')
+            dispatcher.utter_message(text=f'Ok, you chose to buy the fashion item {sku}.')
             return {'sku': sku}
 
     def validate_color(
@@ -360,10 +344,11 @@ class ValidateDetailsForm(FormValidationAction):
         print(result)
 
         if color not in result['color'].iloc[0].lower():
-            dispatcher.utter_message(text="This color is not available for this fashion item")
-            dispatcher.utter_message(text="The colors available for this item are: " + result['color'].iloc[0].lower())
+            dispatcher.utter_message(text="This color is not available for this fashion item.")
+            dispatcher.utter_message(text="The colors available for this item are: " + result['color'].iloc[0].lower()
+                                          + ".")
             return {"color": None}
-        dispatcher.utter_message(text=f"Ok, you chose the color {color} for this fashion item")
+        dispatcher.utter_message(text=f"Ok, you chose the color {color} for this fashion item.")
         return {"color": color}
 
 
@@ -386,10 +371,9 @@ class SubmitDetails(FormValidationAction):
         image = result['image'].iloc[0]
         price = str(result['price'].iloc[0])
 
-        text = "This item will cost you " + price + " dollars.\n" \
-                                                    "By the way, you can have more information about the fashion item you picked in the website.\n" \
-                                                    "Try and visit the url: " + link + "\n" \
-                                                                                       "By the way you have a really good taste in terms of fashion:"
+        text = "This item will cost you " + price + " dollars.\nYou can have more information about the " \
+                                                    "fashion item you picked in the website.\nTry and visit the url: "\
+               + link + "\nBy the way you have a really good taste in terms of fashion:"
 
         dispatcher.utter_message(text=text, image=image)
 
@@ -419,7 +403,8 @@ class ValidateFilterForm(FormValidationAction):
         result = fashion_items[fashion_items['category'].str.lower().str.contains(slot_value)]
 
         if len(result) == 0:
-            dispatcher.utter_message("This category is not available in my catalogue." + "\n")
+            dispatcher.utter_message("This category is not available in my catalogue so this filter will be ignored." +
+                                     "\n")
             dispatcher.utter_message(
                 "What's the maximum you can spend? You can tell me to skip this question by "
                 "typing 'skip'.")
@@ -453,7 +438,7 @@ class ValidateFilterForm(FormValidationAction):
                 "typing 'skip'.")
             return {'price_slot': slot_value}
         else:
-            dispatcher.utter_message("The price value is not valid.")
+            dispatcher.utter_message("The price value is not valid. You have to type a number. Try again.")
             return {'price_slot': None}
 
     def validate_size_slot(
@@ -501,7 +486,7 @@ class SubmitFilter(FormValidationAction):
             result = result[pd.to_numeric(result['price']) <= float(price)]
 
         size = tracker.get_slot('size_slot').lower()
-        if (size != 'no'):
+        if size != 'no':
             result = result[result['size'].str.lower().str.contains(size)]
 
         if len(result) == 0:
@@ -510,11 +495,11 @@ class SubmitFilter(FormValidationAction):
             items = ''
             for ind in result.head(5).index:
                 items = items + f' - ' + str(int(result["sku"][ind])) + ': ' + result["category"][ind] + \
-                        ', ' + str(result['price'][ind]) + '$\n'
+                        ', ' + str(result['price'][ind]) + '$.\n'
             dispatcher.utter_message(text=f"I found {len(result)} results that match your input.\n"
-                                          f"I will show you up to 5 fashion items that I think will fit you:\n" + items +
-                                          "\n\nIf you want, ask me more about a specific item by specifying its "
-                                          "sku code")
+                                          f"I will show you up to 5 fashion items that I think will fit you:\n" + items
+                                          + "\n\nIf you want, ask me more about a specific item by specifying its "
+                                          "sku code.")
 
         return [{"name": "category_slot", "event": "slot", "value": None},
                 {"name": "size_slot", "event": "slot", "value": None},
